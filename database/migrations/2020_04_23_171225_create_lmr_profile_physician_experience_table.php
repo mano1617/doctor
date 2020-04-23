98455771f2b2ac77e0bc5aeb6ec860eb99616b51
Lalbahadur <lal.b@twinsway.in>
2020-04-23 22:56:12 +0530

    tables added

---
+++ database/migrations/2020_04_23_171225_create_lmr_profile_physician_experience_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLmrProfilePhysicianExperienceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lmr_profile_physician_experience', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->mediumText('designation');
+            $table->mediumText('institution');
+            $table->mediumText('place');
+            $table->mediumText('working_years');
+            $table->mediumText('homoeo_experience_years');
+            $table->enum('status',['0','1','2'])->default('1')->comment('0 is deactive, 1 is active, 2 is removed');
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lmr_profile_physician_experience');
+    }
+}
