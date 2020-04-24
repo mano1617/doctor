bb989092bd5c121b5bd9e10d2a2c0e33d5496412
Lalbahadur <lal.b@twinsway.in>
2020-04-24 21:42:58 +0530

    new tables added

---
+++ database/migrations/2020_04_24_155617_create_lmr_phys_clinic_consulting_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLmrPhysClinicConsultingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lmr_phys_clinic_consulting', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('clinic_id');
+            $table->mediumText('name');
+            $table->mediumText('speciality');
+            $table->mediumText('email_address');
+            $table->mediumText('mobile_no');
+            $table->mediumText('monthly_visit');
+            $table->mediumText('others');
+            $table->mediumText('description');
+            $table->enum('status',['0','1','2'])->default('1')->comment('0 is deactive, 1 is active, 2 is removed');
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('clinic_id')->references('id')->on('lmr_physician_clinic');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lmr_phys_clinic_consulting');
+    }
+}
